96d005a7984ef3b15c0c034965e04b649d9c0b98
Zumbisinho <seuemail@example.com>
2026-01-13 18:44:01 -0300

    Finalizando Portifólio

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Portifolio
 Portifólio de habilidades e projetos pessoais do curso instituto evoluir
+
+DeathLine - 12 de janeiro
--- main.js
+++ main.js
@@ -1,2 +1,44 @@
-console.log('Hello Worlid!');
+const root = document.getElementById('cards')
+const template = document.getElementById('template')
 
+function duplicate(imgSrc, name, desc, link, techs, delay) {
+    const clone = template.cloneNode(true);
+    clone.style.display = "flex";
+    clone.querySelector('.template-img').src = imgSrc
+    clone.querySelector('.projeto-descricao h3').innerText = name
+    clone.querySelector('.projeto-descricao p').innerText = desc
+    clone.querySelector('.projeto-descricao a').href = link
+    clone.style.setProperty('--delay', delay + "s");
+
+    function enableTech(TechName) {
+        clone.querySelector(`.${TechName}`).style.display = 'inline-block'
+    }
+    for (const tech of techs) {
+        enableTech(tech)
+    }
+    clone.classList.remove("no-anim");
+    clone.classList.add("anim");
+
+    root.appendChild(clone);
+}
+
+function getList() {
+
+    fetch("projects.json")
+        .then(response => response.json())
+        .then(data => {
+            let i = 1
+            for (let project of data) {
+                duplicate(project['imgUrl'],
+                    project['name'],
+                    project['desc'],
+                    project['link'],
+                    project['techs'],
+                    0.2 * i++
+                )
+            }
+        })
+}
+
+
+getList()
